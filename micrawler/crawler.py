e88c2f8af5734c5adc8c8760100921b37207097c
nebulasibyl98 <sibylsystem98@gmail.com>
2024-07-04 21:28:29 +0800

    优化下载清晰度，优化下载逻辑1.1版本

--- micrawler/crawler.py
+++ micrawler/crawler.py
@@ -3,7 +3,7 @@ from DrissionPage._configs.chromium_options import ChromiumOptions
 from urllib.parse import urlparse, parse_qs
 import json
 
-from colorama import Fore
+from colorama import Fore, Style
 from prettytable import PrettyTable
 from database_utils import log_error_to_db, connect_to_db
 from video_downloader import download_video
@@ -42,35 +42,44 @@ def crawl_page(base_url, start_page, database_config, crawler_config, write_to_e
                     try:
                         img = i('t:img').attr('src') if genre == 'li' else \
                             i.ele('.card-mobile-panel inner').eles('t:img')[1].link
-                        title = i.text if genre == 'li' else i.ele('.card-mobile-title').texts()
+                        title = i.text if genre == 'li' else i.ele('.card-mobile-title').text
                         link = i.href if genre == 'li' else i.ele('t:a').link
                         page2.get(link)
                         hanime1_div = page2.ele('#content-div').ele('#player-div-wrapper')
                         hanime1_div2 = hanime1_div.eles('.video-details-wrapper video-tags-wrapper')
                         author = page2.ele('#content-div').ele(
                             '.video-details-wrapper desktop-inline-mobile-block').ele('#video-artist-name')
-                        print(f'作者: {author.text}')
-
+                        print(Fore.RED + f'作者: {author.text}')
                         for tag in hanime1_div2:
                             print(f'番剧标签: {tag.texts()}')
 
                         parsed_url = urlparse(link)
                         query_params = parse_qs(parsed_url.query)
                         v_value = query_params.get('v', [''])[0]
-                        page3.get('https://hanime1.me/download?v=' + v_value)
-                        download_url = page3.ele('tag:table').eles('tag:a')
-                        vide_data = {}
+                        page3.get(f'https://hanime1.me/download?v={v_value}')
+                        download_urls = page3.ele('tag:table').eles('tag:a')
 
-                        for k in download_url:
-                            if "1080p" in k.link:
-                                vide_data['1080P'] = k.link
-                                print(f'视频下载信息: {vide_data}')
-                                connection = connect_to_db(database_config)
-                                cursor = connection.cursor()
-                                download_video(k, title, v_value, crawler_config, connection, cursor)
-                                break
+                        if not download_urls:
+                            raise Exception("没有找到合适的下载链接")
 
-                        if not vide_data:
+                        video_data = {
+                            res: k.link for k in download_urls for res in ["1080p", "720p", "480p"]
+                            if res in k.link
+                        }
+
+                        if not video_data:
+                            raise Exception("没有找到可用的高清分辨率下载链接")
+
+                        best_quality_link = video_data.get("1080p") or video_data.get("720p") or video_data.get("480p")
+                        print(f'番剧标签: {best_quality_link}')
+                        if best_quality_link:
+                            connection = connect_to_db(database_config)
+                            cursor = connection.cursor()
+                            save_path = f"{crawler_config['download_path']}/{v_value}.mp4"
+                            download_video(best_quality_link, save_path, v_value, crawler_config['max_retries'], cursor, connection)
+                            cursor.close()
+                            connection.close()
+                        else:
                             connection = connect_to_db(database_config)
                             cursor = connection.cursor()
                             log_error_to_db(cursor, v_value, link, "没有找到合适的下载链接")
@@ -80,15 +89,14 @@ def crawl_page(base_url, start_page, database_config, crawler_config, write_to_e
 
                         full_title = f"{title} [v={v_value}]"
                         table.add_row([full_title, link, img])
-
-                        print(f'番剧名称: {full_title}')
+                        print(Fore.CYAN + Style.BRIGHT + f'番剧名称: {full_title}')
                         print(f'番剧URL: {link}')
                         print(f'图片地址: {img}')
                         print('-' * 50)
 
                         json_data = json.dumps(
                             {"番剧名称": full_title, "作者": author.text, "番剧URL": link, "图片地址": img,
-                             "下载地址": vide_data}, ensure_ascii=False, indent=5)
+                             "下载地址": best_quality_link}, ensure_ascii=False, indent=4)
                         print(json_data)
 
                         if write_to_es:
--- micrawler/video_downloader.py
+++ micrawler/video_downloader.py
@@ -1,32 +1,29 @@
 import os
 import requests
 from tqdm import tqdm
-from database_utils import check_and_insert_vide_id, insert_vide_id
+from database_utils import check_and_insert_vide_id, insert_vide_id, check_error_log
 
 
-def download_video(k, title, vide_id, crawler_config, connection, cursor):
-    save_data = f"{title}{vide_id}.mp4"
-    save_path = os.path.join(crawler_config['download_path'], save_data)
-    max_retries = crawler_config['max_retries']
-
+def download_video(download_link, save_path, vide_id, max_retries, cursor, connection):
+    # 检查视频是否已经正常插入，且不在错误日志中
     if check_and_insert_vide_id(cursor, vide_id):
-        cursor.close()
-        connection.close()
-        return
+        if not check_error_log(cursor, vide_id):
+            print(f"视频 {vide_id} 已存在且没有错误日志记录，跳过下载。")
+            cursor.close()
+            connection.close()
+            return
+        else:
+            print(f"视频 {vide_id} 在错误日志中有记录，重新下载。")
 
     retries = 0
     while retries < max_retries:
         try:
-            response = requests.get(k.link, stream=True, timeout=10)
+            response = requests.get(download_link, stream=True, timeout=10)
             response.raise_for_status()
-        except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError,
-                requests.exceptions.Timeout, requests.exceptions.RequestException) as e:
-            print(f"请求错误: {e}")
-            retries += 1
-            continue
 
-        total_size = int(response.headers.get('content-length', 0))
-        try:
+            content_length = response.headers.get('content-length')
+            total_size = int(content_length) if content_length else 0
+
             with open(save_path, 'wb') as file, tqdm(
                     desc="Downloading",
                     total=total_size,
@@ -44,6 +41,12 @@ def download_video(k, title, vide_id, crawler_config, connection, cursor):
                     insert_vide_id(cursor, vide_id)
                     connection.commit()
                 return
+
+        except requests.exceptions.RequestException as e:
+            print(f"请求错误: {e}")
+            retries += 1
+            continue
         except IOError as e:
             print(f"文件写入错误: {e}")
             retries += 1
+    print(f"视频 {vide_id} 下载失败。")
